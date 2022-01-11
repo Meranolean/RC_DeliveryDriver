a3202f722ec6996e1dd1979644d4a9cf944f412c
Meranolean <rumpel135@gmail.com>
2022-01-11 20:23:41 +0300

    Initialization lesson 19

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using System;
 
-public class Driver : MonoBehaviour
-{
-    void Start()
-    {
+public class Driver : MonoBehaviour {
+    [SerializeField] float steerSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    void Start() {
     }
 
-    void Update()
-    {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+    void Update() {
+        float steerAmount = Input.GetAxisRaw("Horizontal") * steerSpeed * Time.deltaTime;
+        // if (System.Math.Abs(Input.GetAxis("Horizontal")) != 1) {
+        //     steerAmount = 0;
+        // }
+        transform.Rotate(0, 0, - steerAmount);
+        float moveAmount = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        // if (System.Math.Abs(Input.GetAxis("Vertical")) != 1) {
+        //     moveAmount = 0;
+        // }
+        // Debug.Log(Input.GetAxis("Vertical"));
+
+        transform.Translate(0, moveAmount, 0);
     }
 }
